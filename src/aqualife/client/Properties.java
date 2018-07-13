5d771d75b2f9f892de754883f0424c8b770acf7e
jualtmey <julian.altmeyer@gmail.com>
2018-07-13 12:03:56 +0200

    refactor

--- src/aqualife/client/Properties.java
+++
@@ -1,8 +0,0 @@
-package aqualife.client;
-
-public class Properties {
-	public final static String HOST = "localhost";
-	public final static int PORT = 4711;
-	public final static String BROKER_NAME = "AquaBroker";
-	public final static int BROKER_THREADS = 5;
-}
